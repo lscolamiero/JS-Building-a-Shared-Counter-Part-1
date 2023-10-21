df1c88afb236ff744977e11c547a8cb637c7762f
lscolamiero <lautaro.scolamiero@gmail.com>
2023-10-21 16:25:38 -0700

    activity done

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+// my work
+    let response = await fetch('http://localhost:9001/counter')
+    let resObj = await response.json()
+    console.log(resObj)
 
-    let countValue = 0;
-
+    let countValue = resObj.value;
+// my work
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
